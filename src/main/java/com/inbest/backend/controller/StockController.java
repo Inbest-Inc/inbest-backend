6f8edcb47d0139a4192621fb2e68b6bf30279a9f
Sude Akarçay <akarcaysude2@gmail.com>
2024-12-22 13:59:31 +0300

    fix(StockController): remove unused import

--- src/main/java/com/inbest/backend/controller/StockController.java
+++ src/main/java/com/inbest/backend/controller/StockController.java
@@ -4,7 +4,6 @@ import com.inbest.backend.service.StockService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
