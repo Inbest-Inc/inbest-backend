f0d921e8a253da0c432a5b1572b43fde024079ff
Gökhan Mert Güneş <gokmert_han@hotmail.com>
2025-01-02 17:56:43 +0300

    Add .env.test and environment configuration for testing

--- src/main/java/com/inbest/backend/BackendApplication.java
+++ src/main/java/com/inbest/backend/BackendApplication.java
@@ -1,5 +1,6 @@
 package com.inbest.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		// .env dosyasını yükler
+		Dotenv dotenv = Dotenv.load();
+
+		// Environment değişkenlerini sistem özelliklerine ekler
+		System.setProperty("POSTGRES_HOST", dotenv.get("POSTGRES_HOST"));
+		System.setProperty("POSTGRES_PORT", dotenv.get("POSTGRES_PORT"));
+		System.setProperty("POSTGRES_DB", dotenv.get("POSTGRES_DB"));
+		System.setProperty("POSTGRES_USER", dotenv.get("POSTGRES_USER"));
+		System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+
+		// Spring Boot uygulamasını başlatır
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
