904272d866d9924e76d9e94f3f565421e474be03
codeyigit <mmehmetkocyigit@hotmail.com>
2025-05-04 20:10:10 +0300

    position metric response fix.

--- src/main/java/com/inbest/backend/service/PortfolioStockMetricService.java
+++ src/main/java/com/inbest/backend/service/PortfolioStockMetricService.java
@@ -1,11 +1,10 @@
 package com.inbest.backend.service;
 
 import com.inbest.backend.model.Portfolio;
+import com.inbest.backend.model.PortfolioStockModel;
 import com.inbest.backend.model.User;
-import com.inbest.backend.repository.PortfolioRepository;
-import com.inbest.backend.repository.PortfolioStockMetricRepository;
-import com.inbest.backend.repository.StockRepository;
-import com.inbest.backend.repository.UserRepository;
+import com.inbest.backend.model.response.PortfolioStockResponse;
+import com.inbest.backend.repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -14,6 +13,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.HashMap;
 
 @Service
 @RequiredArgsConstructor
@@ -22,6 +22,7 @@ public class PortfolioStockMetricService
     private final AuthenticationService authenticationService;
     private final PortfolioRepository portfolioRepository;
     private final PortfolioStockMetricRepository portfolioStockMetricRepository;
+    private final PortfolioStockRepository portfolioStockRepository;
     private final PortfolioService portfolioService;
     private final UserRepository userRepository;
 
@@ -34,7 +35,24 @@ public class PortfolioStockMetricService
         {
             if (portfolioService.checkPortfolioOwnership(portfolioID, user.get().getId()))
             {
-                return portfolioStockMetricRepository.findMetricsByPortfolioId(portfolioID);
+                List<PortfolioStockModel> stocks = portfolioStockRepository.findByPortfolio_PortfolioId((long) portfolioID);
+                return stocks.stream()
+                        .map(stock -> portfolioStockMetricRepository.findTopByPortfolioIdAndStockIdOrderByDateDesc(portfolioID, stock.getStock().getStockId())
+                                .map(metric -> {
+                                    Map<String, Object> map = new HashMap<>();
+                                    map.put("stock_id", metric.getStockId());
+                                    map.put("symbol", stock.getStock().getTickerSymbol());
+                                    map.put("name", stock.getStock().getStockName());
+                                    map.put("shares", metric.getQuantity());
+                                    map.put("allocation", metric.getPositionWeight());
+                                    map.put("averagePrice", metric.getAverageCost());
+                                    map.put("currentPrice", metric.getCurrentValue());
+                                    map.put("return", metric.getTotalReturn());
+                                    return map;
+                                })
+                                .orElse(null))
+                        .filter(metric -> metric != null)
+                        .toList();
             }
         }
         return portfolioStockMetricRepository.findMetricsByPortfolioIdIfPublic(portfolioID);
